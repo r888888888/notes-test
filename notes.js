60303a0a8ec3174dd6cda62faf0c4cd6a787979f
albert <ayi@opnet.com>
2011-05-16 10:30:51 -0400

    added sinatra

--- notes.js
+++ notes.js
@@ -2,12 +2,14 @@ Note = {
   start_translation_mode: function() {
     var note = $('<div></div>');
     note.addClass("note-box");
-    note.draggable({contain: "parent"});
-    note.resizable({contain: "parent"});
+    note.draggable({containment: "parent"});
+    note.resizable({containment: "parent"});
     $("div#note-container").append(note);
   }
 }
 
 $(document).ready(function() {
   $("#translate-button").click(Note.start_translation_mode);
+  $("#note-container").width($("#image").width());
+  $("#note-container").height($("#image").height());
 });
